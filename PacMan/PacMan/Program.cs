1a638a2ccc674c0c508d47be46f7ab4e061f3fef
AzodTM <AzodTM@gmail.com>
2015-03-15 14:51:36 +0500

    Я чот поменял

--- PacMan/PacMan/Program.cs
+++ PacMan/PacMan/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +9,7 @@ namespace PacMan
     {
         static void Main(string[] args)
         {
+		Console.WriteLine("Привет нах");
         }
     }
 }
